dc56f8640fa706a77e308c0ea8d2a6e75146be67
Kirin Dev <hoanglan.casio@gmail.com>
2025-05-02 15:21:28 +0700

    Fix: chạy Flask trên 0.0.0.0 để hợp lệ với Render

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def verify_domain():
     return "zalo-platform-site-verification=Ujs1DQBHEYjWyQ0LcylJDKEzgMk1otysDZ8q", 200
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
